bd468ae06cee71f36cb000dd1d7157d870082bd0
KIRILL F <tiltlessman@gmail.com>
2022-03-07 12:52:58 +0300

    setup React Router v6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Error from './pages/Error'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Dashboard />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='*' element={<Error />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/Error.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Error = () => {
+  return <h3>Error</h3>
+}
+
+export default Error
